19228ca6bce1e96fc1ea8c7cb8f55fce0e5354f3
szbrooks2017 <strattonbrooks13@gmail.com>
2021-08-24 01:05:24 +0000

    Adding task 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array sorted with bubble sort
+ * @size: size of the array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, tmp;
+
+	/* loop through all elements*/
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
