377ea748dfa80f9bacb1d98aefa865906279cabc
Grace Fallon <51837112+angelofgrace@users.noreply.github.com>
2021-08-25 10:54:18 -0500

    hard code to account for size 2 edge case

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@ void selection_sort(int *array, size_t size)
     size_t x, lowIndex, element, subArray;
     int tmp;
 
-    if (array && size > 1) 
+    if (array && size > 2) 
     {
         tmp = array[0];
         lowIndex = 0;
@@ -37,5 +37,15 @@ void selection_sort(int *array, size_t size)
               print_array(array, size);
             }
         }
+    }
+    else if (array && size == 2)
+    {
+      if (array[1] < array[0])
+      {
+        tmp = array[0];
+        array[0] = array[1];
+        array[1] = tmp;
+        print_array(array, size);
+      }
     }
 }
