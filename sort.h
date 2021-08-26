555a26f04a077829faa42ba37ab7584b99d769f3
Grace Fallon <graceleefallon@gmail.com>
2021-08-25 19:23:41 -0500

    add quicksort helper functions to header file

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void sort(int* array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 
 #endif
